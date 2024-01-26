38edc6aa2d63169bdf3812c598a90a7c1bcf6b26
Gabrielly Baiao <gabriellybaiao95@gmail.com>
2024-01-26 11:32:15 -0300

    usando forEach em JS

--- exercicios.js
+++ exercicios.js
@@ -1,14 +1,6 @@
-const notas1 = [10, 6.5, 8, 7.5];
-const notas2 = [9, 6, 6];
-const notas3 = [8.5, 9.5];
+const numeroPares = [];
 
-const notasGerais = [notas1, notas2, notas3];
-let media = 0;
-
-for(i=0; i< notasGerais.length; i++){
-    for(j=0; j<notasGerais[i].length; j++){
-        media += notasGerais[i][j] / notasGerais[i].length;
-    }
+for(let i = 0; i <= 100; i+=2){
+    numeroPares.push(i);
 }
-media = media/notasGerais.length
-console.log(media);
+console.log(numeroPares);
