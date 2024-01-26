30e6a27e39da589cc1dee1bcb6a22144a804843e
Gabrielly Baiao <gabriellybaiao95@gmail.com>
2024-01-26 13:42:23 -0300

    Atribuicao por valor

---
+++ parametros-de-funcoes.js
@@ -0,0 +1,7 @@
+let numeroOriginal = 10;
+
+function alteraNumero(numero){
+    numero = 50;
+    console.log(`número do parâmetro é ${numero}. número original é ${numeroOriginal}`)
+}
+alteraNumero(numeroOriginal)
