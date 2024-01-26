debfedef68c50b365c1fd16616af0b4e97264307
Gabrielly Baiao <gabriellybaiao95@gmail.com>
2024-01-26 15:16:47 -0300

    Entendendo objetos e acessando dados

---
+++ aula-objetosJS/objetos.js
@@ -0,0 +1,10 @@
+const listaCpfs = [111111, 22222, 33333];
+
+const informacoesPessoa = ["nome", "Jose", "idade", 32, "CPF",
+"12131313131"];
+console.log(informacoesPessoa[1]);
+
+const objetoPessoa = {
+    idade: 32,
+    nome: 'Jose',
+}
