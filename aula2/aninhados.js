61b2e6809523cff056bb2103c97610e465e821f5
Gabrielly Baiao <gabriellybaiao95@gmail.com>
2024-01-26 16:36:02 -0300

    Objetos em objetos

---
+++ aula2/aninhados.js
@@ -0,0 +1,13 @@
+const cliente = {
+    nome: "João",
+    idade: 24,
+    email: "joao@dominio.com",
+    telefone: ["11 95555-5555", "11 94444-4444"], 
+};
+cliente.endereco = {
+    rua: "R. Joseph Climber",
+    numero: 1337,
+    apartamento: true,
+    complemento: "Ap 934",
+};
+console.log(cliente.endereco);
