89fc78df29ee0199f65443d2b4f927d80874be26
Gabrielly Baiao <gabriellybaiao95@gmail.com>
2024-01-26 12:56:21 -0300

    Aprendendo a usar o filter

--- exercicios.js
+++ exercicios.js
@@ -4,3 +4,18 @@ for(let i = 0; i <= 100; i+=2){
     numeroPares.push(i);
 }
 console.log(numeroPares);
+
+console.log('-------------------------------');
+const nome = "Alura";
+let nomeMaiusculas = "";
+
+for(let i = 0; i < nome.length; i++ ){
+    nomeMaiusculas += nome[i].toUpperCase();
+}
+console.log(nomeMaiusculas);
+console.log('-----------------------------------------')
+const nomeDoCurso = 'Fundamentos de JS';
+const nomeDaPlataforma = ' Alura';
+
+const nomeCompleto = nomeDoCurso.concat(nomeDaPlataforma);
+console.log(nomeCompleto);
---
+++ map-nomes.js
@@ -0,0 +1,4 @@
+const nomes = ['ana Julia', 'Caio vinicius', 'BIA silva' ];
+
+const nomesPadronizadas =  nomes.map((nome) => nome.toUpperCase());
+console.log(nomesPadronizadas);
