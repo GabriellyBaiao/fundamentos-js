bcb4f3d3dd17867b89642e99f10f25742a9efbd6
Gabrielly Baiao <gabriellybaiao95@gmail.com>
2024-01-26 15:46:53 -0300

    Como deletar uma propriedade

--- exercicios.js
+++ exercicios.js
@@ -1,21 +1,38 @@
-const numeroPares = [];
+// const numeroPares = [];
 
-for(let i = 0; i <= 100; i+=2){
-    numeroPares.push(i);
-}
-console.log(numeroPares);
+// for(let i = 0; i <= 100; i+=2){
+//     numeroPares.push(i);
+// }
+// console.log(numeroPares);
 
-console.log('-------------------------------');
-const nome = "Alura";
-let nomeMaiusculas = "";
+// console.log('-------------------------------');
+// const nome = "Alura";
+// let nomeMaiusculas = "";
 
-for(let i = 0; i < nome.length; i++ ){
-    nomeMaiusculas += nome[i].toUpperCase();
-}
-console.log(nomeMaiusculas);
-console.log('-----------------------------------------')
-const nomeDoCurso = 'Fundamentos de JS';
-const nomeDaPlataforma = ' Alura';
+// for(let i = 0; i < nome.length; i++ ){
+//     nomeMaiusculas += nome[i].toUpperCase();
+// }
+// console.log(nomeMaiusculas);
+// console.log('-----------------------------------------')
+// const nomeDoCurso = 'Fundamentos de JS';
+// const nomeDaPlataforma = ' Alura';
 
-const nomeCompleto = nomeDoCurso.concat(nomeDaPlataforma);
-console.log(nomeCompleto);
+// const nomeCompleto = nomeDoCurso.concat(nomeDaPlataforma);
+// console.log(nomeCompleto);
+// console.log('--------------------------------');
+const objPersonagem = {
+    nome: "Gandalf",
+    classe: "mago",
+    nivel: "20",
+    aliado: {
+      nome: "Saruman",
+      classe: "mago"
+    },
+    status: "desaparecido"
+   }
+   
+const delProp = delete objPersonagem.aliado
+const delPropInexistente = delete objPersonagem["endereco"]
+
+console.log(delProp) //true
+console.log(delPropInexistente) //true
